277fde0fd0e23f3d326b7d7f03b88593a8bc5b62
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-09-22 11:21:01 +0100

    init project

---
+++ hirespace-tech-test/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/header/Header';
+import EventDetails from './components/events/EventDetails';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <div className='event-body'>
+        <EventDetails />
+        <div className='event-hero'>The nice dude</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ hirespace-tech-test/src/components/events/EventDetails.tsx
@@ -0,0 +1,7 @@
+import './eventDetails.css';
+
+const EventDetails = () => {
+  return <div className='event-details'>EventDetails</div>;
+};
+
+export default EventDetails;
